b94ede8787c395d1ff3de12a92ba0516c96816f5
Ignacio Jorquera <ignacio.jorquera@sansano.usm.cl>
2023-07-16 13:58:34 -0400

    clean rtrie

--- ignacio/rtrie/rtrie.cpp
+++ ignacio/rtrie/rtrie.cpp
@@ -1,30 +1,5 @@
 #include "rtrie.h"
 
-// RTrieNode::RTrieNode(char* value, int heigth)
-// {
-//     std::string str_value(value);
-//     this->value = str_value;
-//     this->up_left = nullptr;
-//     this->up_right = nullptr;
-//     this->heigth = heigth;
-// }
-
-// std::string toBinary(int n, unsigned n_bits)
-// {
-//     std::string r;
-
-//     while(n != 0) {
-//         r = (n % 2 == 0 ? "0" : "1") + r;
-//         n = n / 2;
-//     }
-
-//     while (r.length() < n_bits) {
-//         r = "0" + r;
-//     }
-//     // std::cout << r << std::endl;
-//     return r;
-// }
-
 RTrieNode::RTrieNode(std::vector<int> z_order_vector, unsigned start, unsigned end)
 {
     unsigned i, j;
@@ -82,27 +57,6 @@ RTrieNode::RTrieNode(std::vector<int> z_order_vector, unsigned start, unsigned e
             this->down_right = nullptr;
             this->value = false;
         }
-
-        // for (i = 0; i < int_vector.size(); i ++) {
-        //     aux = this;
-        //     std::string binary_number = toBinary(int_vector[i], this->heigth);
-
-        //     for (j = 0; j < this->heigth; j++) {
-        //         if (binary_number[j] == '0') {
-        //             if (!aux->up_left) {
-        //                 aux->up_left = new RTrieNode((char*)"0", this->heigth);
-        //             }
-        //             aux = aux->up_left;
-        //         }
-        //         if (binary_number[j] == '1') {
-        //             if (!aux->up_right) {
-        //                 aux->up_right = new RTrieNode((char*)"1", this->heigth);
-        //             }
-        //             aux = aux->up_right;
-        //         }
-        //     }
-        //     // std::cout << "int_vector[" << i << "]: " << int_vector[i] << " | -> " << toBinary(int_vector[i], heigth) << std::endl;
-        // }
     }
 }
 
@@ -183,70 +137,3 @@ void RTrieNode::printPreorder(RTrieNode* node)
     // Recur on down_right subtree
     printPreorder(node->down_right);
 }
-
-// void zorder(std::vector<int> &vtr, std::vector<std::vector<int>> m, int y0, int x0, int size)
-// {
-//     if (size == 1) {
-//         // Base case, just one cell
-//         vtr.push_back(m[y0][x0]);
-//         // std::cout << "y0: " << y0 << ", x0: " << x0 << ", m[y0][x0]: " << m[y0][x0] << std::endl;
-//     } else {
-//         // Recurse in Z-order
-//         int h = size/2;
-//         zorder(vtr, m, y0,   x0,   h); // top-left
-//         zorder(vtr, m, y0,   x0+h, h); // top-right
-//         zorder(vtr, m, y0+h, x0,   h); // bottom-left
-//         zorder(vtr, m, y0+h, x0+h, h); // bottom-right
-//     }
-// }
-
-
-// unsigned Node::getRTrieEntropy(std::vector<std::vector<int>> binary_matrix) const
-// {
-//     int n_bits = floor(log2(binary_matrix[0].size()));
-//     std::string current_bit_sec, aux, new_bit_sec;
-
-
-//     unsigned trie_entropy = 0;
-//     unsigned x_i, i, j, dif;
-//     bool flag = false;
-
-//     for (i = 0; i < int_vector.size(); i++) {
-//         flag = false;
-//         x_i = 0;
-
-//         if (i == 0) {
-//             current_bit_sec = toBinary(int_vector[i], n_bits);
-//             x_i = current_bit_sec.length();
-//         } else {
-//             aux = toBinary(int_vector[i]);
-//             new_bit_sec = "";
-//             dif = current_bit_sec.length() - aux.length();
-
-//             // Save the prefix of the original value
-//             for (j = 0; j < dif; j++) {
-//                 new_bit_sec += current_bit_sec[j];
-//             }
-
-//             // Start adding bits after the prefix
-//             // if at some point the bit in both secs is different,
-//             // then the prefix is broken and start adding new bits to the sum
-//             for (j = dif; j < current_bit_sec.length(); j++) {
-//                 if (current_bit_sec[j] == aux[j - dif]) {
-//                     new_bit_sec += current_bit_sec[j];
-//                 } else { // Here is the new bit after breaking the prefix
-//                     flag = true;
-//                     new_bit_sec += aux[j - dif];
-//                 }
-//                 if (flag) {
-//                     x_i += 1;
-//                 }
-//             }
-//             current_bit_sec = new_bit_sec;
-//         }
-//         // std::cout << "x_" << i<< ": " << x_i << std::endl;
-//         trie_entropy += x_i;
-//     }
-
-//     return trie_entropy;
-// }
